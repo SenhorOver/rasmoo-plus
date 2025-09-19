22092274acfc32db266bafef513cb9da8803398d
Marcos V Silva <mrcsvs@outlook.com>
2025-09-19 17:59:23 -0300

    feat: create subscription_type model

---
+++ src/main/java/com/client/ws/rasmooplus/model/SubscriptionsType.java
@@ -0,0 +1,39 @@
+package com.client.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "subscriptions_type")
+public class SubscriptionsType implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "subscriptions_type_id")
+    private Long id;
+
+    private String name;
+    @Column(name = "access_month")
+    private Integer accessMonths;
+    private BigDecimal price;
+    @Column(name = "product_key")
+    private String productKey;
+
+    public SubscriptionsType(Long id, String name, Integer accessMonths, BigDecimal price, String productKey) {
+        this.id = id;
+        this.name = name;
+        this.accessMonths = accessMonths;
+        this.price = price;
+        this.productKey = productKey;
+    }
+
+    public SubscriptionsType() {
+    }
+}
