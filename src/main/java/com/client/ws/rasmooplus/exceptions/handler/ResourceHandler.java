8c7d1a24323739658bf72f653ad7083371ca277e
Marcos V Silva <mrcsvs@outlook.com>
2025-09-24 14:40:37 -0300

    feat(error): create error_response_dto and implements in exception handler

--- src/main/java/com/client/ws/rasmooplus/exceptions/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exceptions/handler/ResourceHandler.java
@@ -1,5 +1,6 @@
 package com.client.ws.rasmooplus.exceptions.handler;
 
+import com.client.ws.rasmooplus.dto.error.ErrorResponseDto;
 import com.client.ws.rasmooplus.exceptions.BadRequestException;
 import com.client.ws.rasmooplus.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ResourceHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException err) {
-        String errorMessage = err.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    public ResponseEntity<ErrorResponseDto> notFoundException(NotFoundException err) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto.builder()
+                        .message(err.getMessage())
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                .build());
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> badRequestException(BadRequestException err) {
-        String errorMessage = err.getMessage();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    public ResponseEntity<ErrorResponseDto> badRequestException(BadRequestException err) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
+                .message(err.getMessage())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
     }
 }
