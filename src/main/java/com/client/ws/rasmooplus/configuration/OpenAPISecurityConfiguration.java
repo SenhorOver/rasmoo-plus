79bee2aa5b3c841251b76bc9b8954887680abe61
Marcos V Silva <mrcsvs@outlook.com>
2025-09-25 15:48:21 -0300

    feat(openapi): create swagger initial configuration

---
+++ src/main/java/com/client/ws/rasmooplus/configuration/OpenAPISecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.client.ws.rasmooplus.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info =@Info(
+                title = "User API",
+//                version = "${api.version}",
+                version = "0.0.1",
+                contact = @Contact(
+                        name = "Over", email = "mrcsvs@outlook.com", url = "https://www.github.com/SenhorOver"
+                ),
+                license = @License(
+                        name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                description = "API para para atender ao frontend do RASMOO PLUS"
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenAPISecurityConfiguration {}
