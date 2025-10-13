9f2343bdd4071d83bb4d8102d2128bd435eb8028
Marcos V Silva <mrcsvs@outlook.com>
2025-10-13 18:34:10 -0300

    test: finish raspay integration tests

--- src/test/java/com/client/ws/rasmooplus/integration/WsRaspayIntegrationTest.java
+++ src/test/java/com/client/ws/rasmooplus/integration/WsRaspayIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.client.ws.rasmooplus.integration;
 
 import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
+import com.client.ws.rasmooplus.exceptions.HttpClientException;
 import com.client.ws.rasmooplus.integration.impl.WsRaspayIntegrationImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -45,18 +48,144 @@ class WsRaspayIntegrationTest {
         ReflectionTestUtils.setField(wsRaspayIntegration, "customerUrl", "/customer");
         CustomerDto dto = new CustomerDto();
         dto.setCpf("11122233345");
-        HttpEntity<CustomerDto> request = new HttpEntity<>(dto, this.headers);
+        HttpEntity<CustomerDto> request = new HttpEntity<>(dto, headers);
         when(restTemplate.exchange(
-                "http://localhost:8080/customer",
-                HttpMethod.POST,
-                request,
-                CustomerDto.class)).thenReturn(ResponseEntity.of(Optional.of(dto)));
+                "http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class))
+                .thenReturn(ResponseEntity.of(Optional.of(dto)));
         assertEquals(dto, wsRaspayIntegration.createCustomer(dto));
         verify(restTemplate,times(1)).exchange(
-                "http://localhost:8080/customer",
-                HttpMethod.POST,
-                request,
-                CustomerDto.class);
+                "http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
+
+    }
+
+    @Test
+    void given_createCustomer_when_apiResponseIs400BadRequest_then_returnNull() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "customerUrl", "/customer");
+        CustomerDto dto = new CustomerDto();
+        dto.setCpf("11122233345");
+        HttpEntity<CustomerDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class))
+                .thenReturn(ResponseEntity.badRequest().build());
+        assertNull(wsRaspayIntegration.createCustomer(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
+
+    }
+
+    @Test
+    void given_createCustomer_when_apiResponseGetThrows_then_throwsHttpClientException() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "customerUrl", "/customer");
+        CustomerDto dto = new CustomerDto();
+        dto.setCpf("11122233345");
+        HttpEntity<CustomerDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class))
+                .thenThrow(HttpClientException.class);
+        assertThrows(HttpClientException.class,() -> wsRaspayIntegration.createCustomer(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/customer", HttpMethod.POST, request, CustomerDto.class);
+
+    }
+
+    @Test
+    void given_createOrder_when_apiResponseIs201Created_then_returnOrderDto() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "orderUrl", "/order");
+        OrderDto dto = new OrderDto();
+        dto.setCustomerId("123");
+        HttpEntity<OrderDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class))
+                .thenReturn(ResponseEntity.of(Optional.of(dto)));
+        assertEquals(dto, wsRaspayIntegration.createOrder(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class);
+
+    }
+
+    @Test
+    void given_createOrder_when_apiResponseIs400BadRequest_then_returnNull() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "orderUrl", "/order");
+        OrderDto dto = new OrderDto();
+        dto.setCustomerId("123");
+        HttpEntity<OrderDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class))
+                .thenReturn(ResponseEntity.badRequest().build());
+        assertNull(wsRaspayIntegration.createOrder(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class);
+
+    }
+
+    @Test
+    void given_createOrder_when_apiResponseGetThrows_then_ThrowHttpClientException() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "orderUrl", "/order");
+        OrderDto dto = new OrderDto();
+        dto.setCustomerId("123");
+        HttpEntity<OrderDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class))
+                .thenThrow(HttpClientException.class);
+        assertThrows(HttpClientException.class, () -> wsRaspayIntegration.createOrder(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/order", HttpMethod.POST, request, OrderDto.class);
+
+    }
+
+    @Test
+    void given_processPayment_when_apiResponseIs200Success_then_returnTrue() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "paymentUrl", "/payment");
+        PaymentDto dto = new PaymentDto();
+        dto.setOrderId("321");
+        dto.setCustomerId("123");
+        HttpEntity<PaymentDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class))
+                .thenReturn(ResponseEntity.of(Optional.of(true)));
+        assertTrue(wsRaspayIntegration.processPayment(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class);
+
+    }
+
+    @Test
+    void given_processPayment_when_apiResponseIs400BadRequest_then_returnNull() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "paymentUrl", "/payment");
+        PaymentDto dto = new PaymentDto();
+        dto.setOrderId("321");
+        dto.setCustomerId("123");
+        HttpEntity<PaymentDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class))
+                .thenReturn(ResponseEntity.badRequest().build());
+        assertNull(wsRaspayIntegration.processPayment(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class);
+
+    }
+
+    @Test
+    void given_processPayment_when_apiResponseGetThrow_then_throwsHttpClientException() {
+        ReflectionTestUtils.setField(wsRaspayIntegration, "raspayHost", "http://localhost:8080");
+        ReflectionTestUtils.setField(wsRaspayIntegration, "paymentUrl", "/payment");
+        PaymentDto dto = new PaymentDto();
+        dto.setOrderId("321");
+        dto.setCustomerId("123");
+        HttpEntity<PaymentDto> request = new HttpEntity<>(dto, headers);
+        when(restTemplate.exchange(
+                "http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class))
+                .thenThrow(HttpClientException.class);
+        assertThrows(HttpClientException.class, () -> wsRaspayIntegration.processPayment(dto));
+        verify(restTemplate,times(1)).exchange(
+                "http://localhost:8080/payment", HttpMethod.POST, request, Boolean.class);
 
     }
 
